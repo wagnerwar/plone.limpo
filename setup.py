9b013e3364aac6d287f71243b7b2db4e6033c802
prodam <prodam@localhost.localdomain>
2017-05-04 15:19:34 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='PloneClean',
+      version='1.0',
+      description='BLA BLA BLA',
+      author='eU',
+      author_email='www@bola.com.br',
+      url='https://www.python.org/',
+     )
